e477bcd67113df2c64a3e8c5a8c001376cd76d95
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2020-03-24 00:57:42 +0800

    Reformat lines in AddStudentCommandParserTest

--- src/test/java/tatracker/logic/parser/student/AddStudentCommandParserTest.java
+++ src/test/java/tatracker/logic/parser/student/AddStudentCommandParserTest.java
@@ -50,37 +50,32 @@ public class AddStudentCommandParserTest {
 
         // whitespace only preamble
         assertParseSuccess(parser,
-                PREAMBLE_WHITESPACE + NAME_DESC_BOB
-                        + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + MATRIC_DESC_BOB + TAG_DESC_FRIEND,
+                PREAMBLE_WHITESPACE + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
+                        + MATRIC_DESC_BOB + TAG_DESC_FRIEND,
                 new AddStudentCommand(expectedStudent));
 
         // multiple names - last name accepted
         assertParseSuccess(parser,
-                NAME_DESC_AMY + NAME_DESC_BOB
-                        + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + MATRIC_DESC_BOB + TAG_DESC_FRIEND,
+                NAME_DESC_AMY + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
+                        + MATRIC_DESC_BOB + TAG_DESC_FRIEND,
                 new AddStudentCommand(expectedStudent));
 
         // multiple phones - last phone accepted
-        assertParseSuccess(parser, NAME_DESC_BOB
-                + PHONE_DESC_AMY + PHONE_DESC_BOB
-                + EMAIL_DESC_BOB
-                + MATRIC_DESC_BOB + TAG_DESC_FRIEND,
+        assertParseSuccess(parser,
+                NAME_DESC_BOB + PHONE_DESC_AMY + PHONE_DESC_BOB + EMAIL_DESC_BOB
+                        + MATRIC_DESC_BOB + TAG_DESC_FRIEND,
                 new AddStudentCommand(expectedStudent));
 
         // multiple emails - last email accepted
-        assertParseSuccess(parser, NAME_DESC_BOB
-                + PHONE_DESC_BOB + EMAIL_DESC_AMY
-                + EMAIL_DESC_BOB
-                + MATRIC_DESC_BOB + TAG_DESC_FRIEND,
+        assertParseSuccess(parser,
+                NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_AMY + EMAIL_DESC_BOB
+                        + MATRIC_DESC_BOB + TAG_DESC_FRIEND,
                 new AddStudentCommand(expectedStudent));
 
         // multiple matric numbers - last matric number accepted
-        assertParseSuccess(parser, NAME_DESC_BOB
-                + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + MATRIC_DESC_AMY
-                + MATRIC_DESC_BOB + TAG_DESC_FRIEND,
+        assertParseSuccess(parser,
+                NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + MATRIC_DESC_AMY
+                        + MATRIC_DESC_BOB + TAG_DESC_FRIEND,
                 new AddStudentCommand(expectedStudent));
 
         // multiple tags - all accepted
@@ -106,87 +101,72 @@ public class AddStudentCommandParserTest {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddStudentCommand.MESSAGE_USAGE);
 
         // missing name prefix
-        assertParseFailure(parser, VALID_NAME_BOB
-                        + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                        + MATRIC_DESC_BOB,
+        assertParseFailure(parser,
+                VALID_NAME_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + MATRIC_DESC_BOB,
                 expectedMessage);
 
         // missing phone prefix
-        assertParseFailure(parser, NAME_DESC_BOB
-                        + VALID_PHONE_BOB + EMAIL_DESC_BOB
-                        + MATRIC_DESC_BOB,
+        assertParseFailure(parser,
+                NAME_DESC_BOB + VALID_PHONE_BOB + EMAIL_DESC_BOB + MATRIC_DESC_BOB,
                 expectedMessage);
 
         // missing email prefix
-        assertParseFailure(parser, NAME_DESC_BOB
-                        + PHONE_DESC_BOB + VALID_EMAIL_BOB
-                        + MATRIC_DESC_BOB,
+        assertParseFailure(parser,
+                NAME_DESC_BOB + PHONE_DESC_BOB + VALID_EMAIL_BOB + MATRIC_DESC_BOB,
                 expectedMessage);
 
         // missing matric prefix
-        assertParseFailure(parser, NAME_DESC_BOB
-                        + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                        + VALID_MATRIC_BOB,
+        assertParseFailure(parser,
+                NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + VALID_MATRIC_BOB,
                 expectedMessage);
 
         // all prefixes missing
-        assertParseFailure(parser, VALID_NAME_BOB
-                        + VALID_PHONE_BOB + VALID_EMAIL_BOB
-                        + MATRIC_DESC_BOB,
+        assertParseFailure(parser,
+                VALID_NAME_BOB + VALID_PHONE_BOB + VALID_EMAIL_BOB + MATRIC_DESC_BOB,
                 expectedMessage);
     }
 
     @Test
     public void parse_invalidValue_failure() {
         // invalid name
-        assertParseFailure(parser, INVALID_NAME_DESC
-                + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + MATRIC_DESC_BOB
-                + TAG_DESC_HUSBAND
-                + TAG_DESC_FRIEND, Name.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser,
+                INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB + MATRIC_DESC_BOB
+                        + TAG_DESC_HUSBAND + TAG_DESC_FRIEND,
+                Name.MESSAGE_CONSTRAINTS);
 
         // invalid phone
         assertParseFailure(parser,
-                NAME_DESC_BOB + INVALID_PHONE_DESC
-                        + EMAIL_DESC_BOB + MATRIC_DESC_BOB
-                + TAG_DESC_HUSBAND + TAG_DESC_FRIEND,
+                NAME_DESC_BOB + INVALID_PHONE_DESC + EMAIL_DESC_BOB + MATRIC_DESC_BOB
+                        + TAG_DESC_HUSBAND + TAG_DESC_FRIEND,
                 Phone.MESSAGE_CONSTRAINTS);
 
         // invalid email
-        assertParseFailure(parser, NAME_DESC_BOB
-                + PHONE_DESC_BOB + INVALID_EMAIL_DESC
-                + MATRIC_DESC_BOB
-                + TAG_DESC_HUSBAND
-                + TAG_DESC_FRIEND, Email.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser,
+                NAME_DESC_BOB + PHONE_DESC_BOB + INVALID_EMAIL_DESC + MATRIC_DESC_BOB
+                        + TAG_DESC_HUSBAND + TAG_DESC_FRIEND,
+                Email.MESSAGE_CONSTRAINTS);
 
         // invalid matric
-        assertParseFailure(parser, NAME_DESC_BOB
-                + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + INVALID_MATRIC_DESC
-                + TAG_DESC_HUSBAND + TAG_DESC_FRIEND,
+        assertParseFailure(parser,
+                NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + INVALID_MATRIC_DESC
+                        + TAG_DESC_HUSBAND + TAG_DESC_FRIEND,
                 Matric.MESSAGE_CONSTRAINTS);
 
         // invalid tag
         assertParseFailure(parser,
-                NAME_DESC_BOB
-                        + PHONE_DESC_BOB
-                        + EMAIL_DESC_BOB
-                        + MATRIC_DESC_BOB
-                + INVALID_TAG_DESC
-                        + VALID_TAG_FRIEND, Tag.MESSAGE_CONSTRAINTS);
+                NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + MATRIC_DESC_BOB
+                        + INVALID_TAG_DESC + VALID_TAG_FRIEND,
+                Tag.MESSAGE_CONSTRAINTS);
 
         // two invalid values, only first invalid value reported
         assertParseFailure(parser,
-                INVALID_NAME_DESC + PHONE_DESC_BOB
-                        + EMAIL_DESC_BOB + MATRIC_DESC_BOB,
+                INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB + MATRIC_DESC_BOB,
                 Name.MESSAGE_CONSTRAINTS);
 
         // non-empty preamble
         assertParseFailure(parser,
-                PREAMBLE_NON_EMPTY + NAME_DESC_BOB
-                        + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + MATRIC_DESC_BOB + TAG_DESC_HUSBAND
-                        + TAG_DESC_FRIEND,
+                PREAMBLE_NON_EMPTY + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
+                        + MATRIC_DESC_BOB + TAG_DESC_HUSBAND + TAG_DESC_FRIEND,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddStudentCommand.MESSAGE_USAGE));
     }
 }
